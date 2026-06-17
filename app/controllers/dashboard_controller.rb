c7b3629cf57cefbefbe01ff39d948ddaba147af1
Vijay <vjkgaikwad@yahoo.com>
2026-06-17 19:13:25 +0530

    Fix passkey props path and dashboard test failures

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -15,20 +15,19 @@ class DashboardController < Sellers::BaseController
     else
       LargeSeller.create_if_warranted(current_seller)
       presenter = CreatorHomePresenter.new(pundit_user)
-      props_data = presenter.creator_home_props.merge(
-        show_passkey_prompt: current_user.passkey_prompt_eligible? && current_user.role_owner_for?(current_seller)
-      )
 
       render inertia: "Dashboard/Index",
-             props: { creator_home: props_data }
+             props: {
+               creator_home: presenter.creator_home_props,
+               prompt_passkey_setup: current_user.passkey_prompt_eligible? && current_user.role_owner_for?(current_seller)
+             }
     end
   end
 
   def dismiss_passkey_prompt
     authorize :dashboard, :index?
-
-    current_user.update!(passkey_prompt_dismissed_at: Time.current)
-
+    refresh_passkey_setup_prompt(current_user)
+    current_user.update_column(:passkey_prompt_dismissed_at, Time.current)
     head :ok
   end
 
